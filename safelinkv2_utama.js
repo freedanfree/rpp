b3b19f968b70cbd568a296462bd9f92178db8c69
freedanfree <38255703+freedanfree@users.noreply.github.com>
2018-08-18 14:23:30 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -21,10 +21,12 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://kompisafelinkv2.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="prinsip-pengembangan-silabus.html"}
+if(x=="2"){xxx="menurut-pengertian-dasar-silabus-dan.html"}
+if(x=="3"){xxx="rpp-rencana-pelaksanaan-pembelajaran.html"}
+if(x=="4"){xxx="rpp-merupakan-persiapan-yang-harus.html"}
+if(x=="5"){xxx="pelaksanaan-pembelajaran-rpp-silabus.html"}
+if(x=="6"){xxx="prinsip-pengembangan-silabus.html"}a_to_ve[i].href="https://www.doraemonindonesia.me/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
